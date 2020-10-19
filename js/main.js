7bb8e45c8f340df3a25f3e0ae8e9ca10620dc50d
Marouane <d144013152@gmail.com>
2020-10-19 21:37:40 +0100

    Create Featured Work

--- js/main.js
+++ js/main.js
@@ -5,4 +5,16 @@ $(function(){
     var Uph = $('.upper-bar').innerHeight();
     var NavH = $('.navbar').innerHeight();
     $('.slider .carousel-item').height( WinH - ( Uph + NavH ));
+
+    // Featured Work
+    $('.featured-work ul li').on('click', function(){
+        $(this).addClass('active').siblings().removeClass('active');
+        if ( $(this).data('class') === 'all' ){
+            $('.shuffle-images .row .col-md').css('opacity', '1');
+        }
+        else {
+            $('.shuffle-images .row .col-md').css('opacity', '.08');
+            $($(this).data('class')).parent().css('opacity', '1');
+        }
+    })
 });
